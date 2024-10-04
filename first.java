5ddb3eb10ecd43249ea3b1da7c26ec350f551ac1
karishma1602 <156992298+karishma1602@users.noreply.github.com>
2024-10-04 15:51:12 +0530

    Update first.java
    add  new messsage

--- first.java
+++ first.java
@@ -2,5 +2,8 @@ public  class student{
 	public static void main(String[]args){
 		System.out.println("hello world!!");
 		System.out.println("git lecture");
+		System.out.println("basic changes");
+		System.out.println("friday");
+		
 	}
 }
